01c95b8a021cdc1c7b2f289778fa8eaf7e34a910
Michael Sanford <mikethecodegeek@gmail.com>
2016-08-03 13:04:51 -0700

    stiff

--- public/js/module.js
+++ public/js/module.js
@@ -1,7 +1,7 @@
 
 'use strict';
 
-var app = angular.module('angularApp', ['ui.router', 'satellizer', 'ngPassword', 'ngAnimate']);
+var app = angular.module('angularApp', ['ui.router', 'satellizer', 'ngAnimate']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 
